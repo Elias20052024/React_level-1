595ba781a6cab899049e1e61fc654dc743ab1596
Mohammad Elias <meprogramer2024@gmail.com>
2024-09-15 16:28:39 +0800

    first commit

---
+++ src/comp/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      Designed and developed by Mohammd Elias
+      <span>🧡</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/home.js
@@ -0,0 +1,24 @@
+
+import Header from '../comp/header';
+import Footer from '../comp/Footer';
+
+
+
+
+const Home = () => {
+  return (
+    <>
+
+    <Header />   
+
+   
+    
+    <main>
+      Home Page
+    </main>
+    <Footer />
+  </>
+  );
+}
+
+export default Home;
